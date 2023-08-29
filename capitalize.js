f43cf492689999ffccad28782eea8cb8b061fe40
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-29 15:05:09 -0500

    Write test and apply function

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,7 @@
-const capitalize = (string) => {};
+const capitalize = (str) => {
+  let allLower = str.toLowerCase();
+  let capitalized = str.charAt(0).toUpperCase() + allLower.slice(1);
+  return capitalized;
+};
 
 module.exports = capitalize;
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,1 +1,5 @@
 const capitalize = require('./capitalize');
+
+test.skip('Returns capitalized string', () => {
+  expect(capitalize('hello')).toBe('Hello');
+});

d256e7962468224cab0f2d1d79434fa4a988e6c1
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-29 17:35:37 -0500

    Add min, max, length funcs

--- analyzeArray.js
+++ analyzeArray.js
@@ -3,9 +3,9 @@ const { cursorTo } = require('readline');
 const analyzeArray = (arr) => {
   const result = {
     average: _findAverage(arr),
-    min: null,
-    max: null,
-    length: null,
+    min: _findMin(arr),
+    max: _findMax(arr),
+    length: _findLength(arr),
   };
   return result;
 };
@@ -15,6 +15,19 @@ function _findAverage(arr) {
     return total + current;
   }, 0);
   return sum / arr.length;
+}
+
+function _findMin(arr) {
+  const min = Math.min(...arr);
+  return min;
+}
+function _findMax(arr) {
+  const max = Math.max(...arr);
+  return max;
+}
+function _findLength(arr) {
+  const length = arr.length;
+  return length;
 }
 
 module.exports = analyzeArray;

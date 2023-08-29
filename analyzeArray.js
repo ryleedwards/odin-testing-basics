78d85ed4f972470c21da262ec6c3d4d28897230d
Ryle Edwards <ryle.edwards@gmail.com>
2023-08-29 17:31:10 -0500

    Add _findAverage func

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,6 +1,20 @@
+const { cursorTo } = require('readline');
+
 const analyzeArray = (arr) => {
-  const result = { average: null, min: null, max: null, length: null };
+  const result = {
+    average: _findAverage(arr),
+    min: null,
+    max: null,
+    length: null,
+  };
   return result;
 };
+
+function _findAverage(arr) {
+  const sum = arr.reduce((total, current) => {
+    return total + current;
+  }, 0);
+  return sum / arr.length;
+}
 
 module.exports = analyzeArray;
